8dfd9b885e0ec1f73633ae33c56b50cef5fefd3f
Marius <marius.kub@outlook.com>
2023-06-17 10:56:35 +0100

    added header with img and border effect, also added react modal for image.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+
+// Components
+import Header from './components/Header/header';
+
+
 // Bootstrap
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
+// CSS
+import './assets/styles.css'
 
 
 function App() {
   return <>
     <Container fluid>
+      <Row>
+        <Header />
+      </Row>
       <Row>
         <Router>
             <Routes>
