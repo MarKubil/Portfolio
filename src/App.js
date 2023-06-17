cbdf08545d57aae0d27d178b3c8550c44cd245de
Marius <marius.kub@outlook.com>
2023-06-17 10:40:48 +0100

    Installed bootstrap and router, Created website structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// React
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Bootstrap
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <Container fluid>
+      <Row>
+        <Router>
+            <Routes>
+              <Route exact path="/" element="" />
+              <Route path="about" element=""/>
+            </Routes>
+        </Router>
+      </Row>
+    </Container>
+  </>;
 }
 
 export default App;
