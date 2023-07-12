3c054746ad3d6a0afb7e125c006be58560c8464e
Marius <marius.kub@outlook.com>
2023-07-12 08:56:58 +0100

    added weather forecast project and colour factory project to the grid

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,23 +1,30 @@
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 import "./styles.css"
 
+// Images
+import WeatherAppImg from '../../assets/img/weather-app.png'
+
+
 function Projects() {
+
     return (
         <div className="box">
             <div className="content">
                 <div className="row">
-                    <div className="col-lg-6 col-sm-12 p-cont">
-                    
-                    </div>
-                    <div className="col-lg-6 col-sm-12 p-cont">
-                    
-                    </div>
+                    <a target="_blank" href="https://google.lt" id="weather-app" className="col-lg-6 col-sm-12 p-cont">
+                        <h4 className="p-description">Weather forecast application</h4>
+                    </a>
+                    <a target="_blank" href="https://thecolourfactory.netlify.app/" id="colour-factory" className="col-lg-6 col-sm-12 p-cont">
+                        <h4 className="p-description">Colour factory application</h4>
+                    </a>
                 </div>
                 <div className="row">
                     <div className="col-lg-6 col-sm-12 p-cont">
-                    
+
                     </div>
                     <div className="col-lg-6 col-sm-12 p-cont">
-                    
+
                     </div>
                 </div>
             </div>
